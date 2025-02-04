26ad55fed231287e271cf37ea34cb3fc226db6f2
Francesca Salute <francescasalute@hotmail.it>
2025-02-04 15:48:10 +0100

    building llama framework

---
+++ llama_chatbot.py
@@ -0,0 +1,7 @@
+################################################################################
+#  CHATBOT WITH LLAMA MODEL   #
+################################################################################
+
+'''Reason: use internal chatbot based on Llama model instead of ChatGPT or any other third party model
+mainly because of privacy reasons, it's good that we don't have to send the data to any third party server and 
+we have full control over the data.'''
