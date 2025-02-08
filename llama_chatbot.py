f76aa27bc6623905308814e1fd7d1c1093eb0306
Francesca Salute <francescasalute@hotmail.it>
2025-02-08 12:53:15 +0100

    implementing graph

--- llama_chatbot.py
+++ llama_chatbot.py
@@ -5,3 +5,23 @@
 '''Reason: use internal chatbot based on Llama model instead of ChatGPT or any other third party model
 mainly because of privacy reasons, it's good that we don't have to send the data to any third party server and 
 we have full control over the data.'''
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from sentence_transformers import SentenceTransformer
+import networkx as nx  # this lib is for graphRAG 
+import numpy as np
+
+# Loading Llama 3.3 model from Hugging Face because of storage limitations
+
+def load_llama_model():
+    '''Load Llama 3.3 model from Hugging Face'''
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.3-70B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.3-70B-Instruct")
+    return tokenizer, model
+
+# Setting up GraphRAG retrieval
+
+def get_graphrag():
+    '''Load GraohRAG model for retrieval of data from CBS Archive'''
+    
